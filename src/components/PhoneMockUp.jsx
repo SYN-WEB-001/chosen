cc90eaa88ea9bbbd905b0e05b4a08c06bdb0f2aa
Zirawan salim <zirawan@hotmail.com>
2025-12-02 16:44:14 +0100

    korektur

---
+++ src/components/PhoneMockUp.jsx
@@ -0,0 +1,25 @@
+
+import Card from "./Card.jsx";
+import { useState } from 'react'
+
+export default function PhoneMockUp() {
+
+    return (
+        <>
+            <div className="mockup-phone border-[#ff8938] rotate-5 shadow-2xl">
+                <div className="mockup-phone-camera"></div>
+                <div className="mockup-phone-display">
+
+                    <div className="mockup-phone-display bg-base-100 pb-10">
+                        <div className="h-full overflow-hidden">
+                            <Card />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </>
+
+    );
+}
