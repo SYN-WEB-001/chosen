f7a68cfc0455b580e66534b58a936d2ee45bd36d
Hakan Öztürk <hakanoezuerk@gmail.com>
2025-12-03 13:37:27 +0100

    hgftjgcjk

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import PhoneMockUp from "./PhoneMockUp";
 export default function HeroSection() {
     return (
         <section
-            className="hero-section relative flex flex-row text-black py-20 overflow-hidden"
+            className="hero-section relative flex flex-row  text-black py-20 overflow-hidden"
             style={{
                 backgroundImage: "url('/Hintergrundbild.jpg')",
                 backgroundSize: "cover",
